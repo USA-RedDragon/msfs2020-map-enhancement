ee10e546e426416f2df005c64d53d90266d8e02b
Jacob McSwain <jacob.a.mcswain@gmail.com>
2022-04-24 00:38:24 -0500

    Make installer patch embedded python's path

---
+++ build/patch_python.ps1
@@ -0,0 +1,7 @@
+$InternalPythonShort = "39"
+$installDir = $args[0]
+$serverPath = "$installDir/resources/extra/server"
+$pythonPath = "$serverPath/python"
+
+# Add installed python directory to ._pth file
+(Get-Content "$pythonPath/python$InternalPythonShort._pth") | Foreach-Object {$_ -replace "^Lib/site-packages$", "$serverPath`nLib/site-packages"} | Set-Content "$pythonPath/python$InternalPythonShort._pth"
